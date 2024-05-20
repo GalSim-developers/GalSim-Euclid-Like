c9aff26eeba11a44c835ec12bc49b03ef422e36d
aguinot <axel.guinot.astro@gmail.com>
2024-05-20 16:20:12 -0400

    Move euclidlike_psf.py to euclidlike

--- euclidlike/__init__.py
+++ euclidlike/__init__.py
@@ -1,6 +1,4 @@
 from .instrument_params import gain, pixel_scale, diameter, obscuration, collecting_area
 from .instrument_params import long_exptime, short_exptime, read_noise, n_dithers
 from .instrument_params import n_ccd, n_pix_row, n_pix_col, pixel_scale_mm
-
-
-
+from ..euclidlike import euclidlike_psf
