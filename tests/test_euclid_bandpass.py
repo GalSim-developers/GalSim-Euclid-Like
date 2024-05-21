93c4e47d137fae97afa1bce32795cba8b4890562
Andy Park <chanhyup@andrew.cmu.edu>
2024-05-21 12:44:06 -0400

    Add euclid bandpass test

---
+++ tests/test_euclid_bandpass.py
@@ -0,0 +1,16 @@
+import numpy as np
+import galsim
+from euclidlike import bandpass
+
+def test_euclid_bandpass():
+    # Obtain the bandpasses with AB_zeropoint set
+    bp = bandpass.getBandpasses(AB_zeropoint=True)
+
+    # Check if the zeropoints have been set correctly
+    AB_spec = lambda x: (3631e-23)
+    AB_sed = galsim.SED(spec=AB_spec, wave_type='nm', flux_type='fnu')
+    for filter_name, filter_ in bp.items():
+        mag = AB_sed.calculateMagnitude(bandpass=filter_)
+        np.testing.assert_almost_equal(mag,0.0,decimal=6,
+            err_msg="Zeropoint not set accurately enough for bandpass filter "+filter_name)
+    return
