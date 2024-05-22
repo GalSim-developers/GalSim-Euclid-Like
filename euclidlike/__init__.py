de0522e6848ddcf5017f180e37403bd0f106dc05
Xiangchong Li <xiangchong.li@ipmu.jp>
2024-05-23 04:37:02 +0900

    fix typo in instrumentparams

--- euclidlike/__init__.py
+++ euclidlike/__init__.py
@@ -2,6 +2,7 @@ from .instrument_params import gain, pixel_scale, diameter, obscuration, collect
 from .instrument_params import focal_length, fratio
 from .instrument_params import long_exptime, short_exptime, read_noise, n_dithers
 from .instrument_params import n_ccd, n_pix_row, n_pix_col, pixel_scale_mm
+from . import instrument_params
 from .bandpass import getBandpasses
 
 from .euclidlike_psf import (
