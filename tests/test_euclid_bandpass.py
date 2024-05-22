55ba6fc7867f9b0c97cd21f1bcd6d2bb76fc3326
Andy Park <chanhyup@andrew.cmu.edu>
2024-05-22 09:50:37 -0400

    Fix typo

--- tests/test_euclid_bandpass.py
+++ tests/test_euclid_bandpass.py
@@ -1,10 +1,10 @@
 import numpy as np
 import galsim
-from euclidlike import bandpass
+import euclidlike
 
 def test_euclid_bandpass():
     # Obtain the bandpasses with AB_zeropoint set
-    bp = bandpass.getBandpasses(AB_zeropoint=True)
+    bp = euclidlike.getBandpasses(AB_zeropoint=True)
 
     # Check if the zeropoints have been set correctly
     AB_spec = lambda x: (3631e-23)
@@ -15,7 +15,7 @@ def test_euclid_bandpass():
             err_msg="Zeropoint not set accurately enough for bandpass filter "+filter_name)
 
     # Check if the zeropoints have not been set
-    nozp_bp = galsim.roman.getBandpasses(AB_zeropoint=False)
+    nozp_bp = euclidlike.getBandpasses(AB_zeropoint=False)
     for key in nozp_bp:
         assert nozp_bp[key].zeropoint is None
     return
