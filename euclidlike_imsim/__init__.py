5064d17b49063873e06830ef2bc7466fd3eb45b5
Andy Park <chanhyup@andrew.cmu.edu>
2024-07-17 22:23:20 +0200

    Change __init__.py file and comment out detector effect as we don't have any for euclidlike

--- euclidlike_imsim/__init__.py
+++ euclidlike_imsim/__init__.py
@@ -5,10 +5,10 @@ except:
     pass
 from .obseq import *
 from .psf import *
-from .sca import *
+from .ccd import *
 from .stamp import *
 from .wcs import *
 from .skycat import *
 from .photonOps import *
 from .bandpass import *
-from .detector_physics import *
+# from .detector_physics import *
