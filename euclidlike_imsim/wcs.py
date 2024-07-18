c1a679c652cad03d1a4963c2eff3df5fbef87814
aguinot <axel.guinot.astro@gmail.com>
2024-07-18 10:08:15 -0400

    Add support for min_sun_angle

--- euclidlike_imsim/wcs.py
+++ euclidlike_imsim/wcs.py
@@ -17,7 +17,11 @@ class EuclidlikeWCS(WCSBuilder):
             "pa": Angle,
             "mjd": float,
         }
-        opt = {"max_sun_angle": float, "force_cvz": bool}
+        opt = {
+            "min_sun_angle": float,
+            "max_sun_angle": float,
+            "force_cvz": bool,
+        }
 
         kwargs, safe = galsim.config.GetAllParams(
             config,
@@ -25,20 +29,22 @@ class EuclidlikeWCS(WCSBuilder):
             req=req,
             opt=opt,
         )
-        if "max_sun_angle" in kwargs:
+        if "min_sun_angle" in kwargs:
             euclidlike.instrument_params.min_sun_angle = \
-                kwargs["min_sun_angle"]
+                kwargs["min_sun_angle"] * galsim.degrees
             euclidlike.euclidlike_wcs.min_sun_angle = \
-                kwargs["min_sun_angle"]
+                kwargs["min_sun_angle"] * galsim.degrees
+        if "max_sun_angle" in kwargs:
             euclidlike.instrument_params.max_sun_angle = \
-                kwargs["max_sun_angle"]
-            euclidlike.euclidlike_wcs.max_sun_angle = kwargs["max_sun_angle"]
+                kwargs["max_sun_angle"] * galsim.degrees
+            euclidlike.euclidlike_wcs.max_sun_angle = \
+                kwargs["max_sun_angle"] * galsim.degrees
         pointing = CelestialCoord(ra=kwargs["ra"], dec=kwargs["dec"])
         wcs = euclidlike.getWCS(
             world_pos=pointing,
             PA=kwargs["pa"],
             date=Time(kwargs["mjd"], format="mjd").datetime,
-            SCAs=kwargs["CCD"],
+            CCDs=kwargs["CCD"],
             PA_is_FPA=True,
         )[kwargs["CCD"]]
         return wcs
