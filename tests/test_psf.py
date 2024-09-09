f3ab025df0f2560d2c34c1a2affed47d10bb7573
Federico Berlfein <berlfein@m005.opa.vera.psc.edu>
2024-09-09 13:16:15 -0400

    Testing symlink Github Actions

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -26,7 +26,7 @@ def test_get_psf_function():
 
     ## testing symlink failure
     #os.symlink(target_file, symlink_file)
-    psfobjs = euclidlike.euclidlike_psf._make_psf_list("../psfs/monopsfs_6_6.fits.gz")
+    psfobjs = euclidlike.euclidlike_psf._make_psf_list("psfs/monopsfs_6_6.fits.gz")
     contents = os.listdir(psf_dir)
     print("Contents of directory:")
     pyfits.getdata(psf_file)
