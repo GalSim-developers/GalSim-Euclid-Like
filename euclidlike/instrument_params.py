db10d61b9cc1f14d337efad606ea6273861d1094
FedericoBerlfein <107272465+FedericoBerlfein@users.noreply.github.com>
2024-09-27 10:25:08 -0400

    Updated saturation limit value

--- euclidlike/instrument_params.py
+++ euclidlike/instrument_params.py
@@ -18,7 +18,7 @@ long_exptime = 566  # s (for the longer exposures used for VIS images)
 short_exptime_nisp = 112  # s (for the shorter NISP imaging exposures)
 short_exptime_vis = 95 # s (for the shorter exposures with VIS taken in parallel with NISP imaging)
 read_noise = 4.4  # e-, https://www.euclid-ec.org/public/mission/vis/
-saturation = 1e5 # in e-, placeholder value for VIS
+saturation = 200000 # e-, from https://www.euclid-ec.org/public/mission/vis/#:~:text=VIS%20pixels%20are%20saturated%20at,IE(AB)%3D16.0.
 n_dithers = 4
 n_ccd = 36
 n_ccd_row = 6
