38fdf44e6829b61c317f194be07b96c9b72e3869
Axel Guinot <39480528+aguinot@users.noreply.github.com>
2024-12-11 15:55:41 -0500

    Update euclidlike_imsim/noise.py
    Co-authored-by: Rachel Mandelbaum <rmandelb@andrew.cmu.edu>

--- euclidlike_imsim/noise.py
+++ euclidlike_imsim/noise.py
@@ -49,7 +49,7 @@ def get_noise(cfg_noise, cfg_image, base, logger):
     NOTE on the quantization: This prevents problems due to the rounding.
     For example, given the very low amplitude of the sky background, we can have
     spatial variations of only ~1 ADU. This will be impossible to properly pick
-    up by tools SExtractor. Adding this noise prvent this problem and do not
+    up by tools like SExtractor. Adding this noise prevents this problem and does not
     change the signal in the image. This is discuss in [Cuillandre et al. 2025](https://arxiv.org/abs/2405.13496)
     Sect. 4.2.7.
     """
