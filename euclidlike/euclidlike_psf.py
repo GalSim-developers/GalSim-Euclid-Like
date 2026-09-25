8c30d5506df33d06aa699635d3c3cb284112cec9
aguinot <axel.guinot.astro@gmail.com>
2026-09-25 14:39:40 -0400

    Add explanation regarding the psf_shift

--- euclidlike/euclidlike_psf.py
+++ euclidlike/euclidlike_psf.py
@@ -125,6 +125,11 @@ def getPSF(
     PSF images by collecting_area / ((diameter/2)**2*np.pi).  As a result, the sum of the pixel
     values in the renormalized PSF images is very close to 1.
 
+    The provided PSF images are off-centered by (-0.5, -0.5) pixels. Galsim assume that the PSF is centered so
+    we are shifting the back the PSF after the interpolation. Not doing this lead to a systematic shift of the
+    object position in the image with respect to the true position. This is drived by the `psf_shift`
+    argument.
+
     Members of the Euclid Consortium with access to the internal PSF toolkit can reproduce the
     precomputed PSF images using the `Develop` branch as of 21 May 2024 using the config file
     `data/config.zip`.
